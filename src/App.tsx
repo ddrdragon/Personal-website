2cc98171d38096dc24690867f727faa6f6d71e0a
xd59 <xd59@njit.edu>
2021-06-04 18:36:35 -0400

    hello

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Hello, Xiaotong!
         </p>
         <a
           className="App-link"
